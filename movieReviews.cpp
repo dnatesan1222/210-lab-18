032502812bce880109612f9e8437e69890cf7ed9
Diksha <diksha@macbookpro>
2025-10-05 15:38:01 -0700

    head() complete, starting tail()

--- movieReviews.cpp
+++ movieReviews.cpp
@@ -25,16 +25,31 @@ int main(){
 
 void head(Node *&head, double r, string c){
     Node *n = new Node;
-    n->rating = r;
+    n->rating = r;		//save the values in a new node
     n->comment = c;
 
-    if (!head){
-        n->next = nullptr;
-        head = n;
-        return;
-    }else
-        n->next = head;
+    if (!head)			//checks if tthe orig LL has nodes yet
+        n->next = nullptr;	//if not, the new node is the first node of the LL
+    else
+        n->next = head;		//if so, new node becomes the head and points to LL
+    head = n;			//save the new head to the original address 
 
+}
+
+void tail(Node *&head, double r, string c){
+    Node *tail = new Node;
+    tail->rating = r;
+    tail->comment = c;
+    tail->next = nullptr;
+
+    if(!head)
+        head = tail;
+    else{
+        Node *temp = head;
+        while (temp)
+            temp = temp->next;
+        
+    }
 
 }
 
