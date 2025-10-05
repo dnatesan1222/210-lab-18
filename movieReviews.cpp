345092647f640de2542d4a1a7d5ed9331f738155
Diksha <diksha@macbookpro>
2025-10-05 15:17:40 -0700

    starting func prototypes

--- movieReviews.cpp
+++ movieReviews.cpp
@@ -1,2 +1,25 @@
 // COMSC-210 | Lab 18 | Diksha Tara Natesan
 // IDE used: Vim/Terminal
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Node {
+    double rating;
+    string comment;
+    Node* next;
+}
+
+//function prototypes
+
+void display(Node *)
+
+
+int main(){
+    //ask user whether 
+
+}
+
+
